21daa0bd6b4110e9cc5aeeb5acba9a3b8f09ef3e
Brian Faust <hello@basecode.sh>
2020-09-20 16:47:57 +0300

    wip

--- src/DTO/Rate.php
+++ src/DTO/Rate.php
@@ -11,7 +11,7 @@ declare(strict_types=1);
  * file that was distributed with this source code.
  */
 
-namespace KodeKeep\CommonCryptoExchange\Enums;
+namespace KodeKeep\CommonCryptoExchange\DTO;
 
 use Illuminate\Support\Carbon;
 use Spatie\DataTransferObject\DataTransferObject;
--- src/DTO/Ticker.php
+++ src/DTO/Ticker.php
@@ -11,7 +11,7 @@ declare(strict_types=1);
  * file that was distributed with this source code.
  */
 
-namespace KodeKeep\CommonCryptoExchange\Enums;
+namespace KodeKeep\CommonCryptoExchange\DTO;
 
 use Spatie\DataTransferObject\DataTransferObject;
 
